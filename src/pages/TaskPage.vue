45f9ce36d4e66fd4473f8b291163b646631057b3
M1 <studio@M1s-Mac-mini.local>
2026-03-22 15:56:08 +0700

    80. Creating an Inline editing feature

--- src/pages/TaskPage.vue
+++ src/pages/TaskPage.vue
@@ -4,51 +4,67 @@
             <div class="row">
                 <div class="col-md-8 offset-md-2">
                     <!-- Add new Task -->
-                    <div class="relative">
-                        <input
-                            type="text"
-                            class="form-control form-control-lg padding-right-lg"
-                            placeholder="+ Add new task. Press enter to save."
-                        />
-                    </div>
+                    <NewTask @added="handleAddedTask" />
+
                     <!-- List of uncompleted tasks -->
                     <Tasks :tasks="uncompletedTasks" />
-                    
+
                     <!-- show toggle button -->
-                     <div class="text-center my-3" v-show="showToggleCompletedBtn">
-                        <button class="btn btn-sm btn-secondary" 
-                        @click="$event => showCompletedTasks = !showCompletedTasks">
-                        <span v-if="!showCompletedTasks">Show completed</span>
-                        <span v-else>Hide completed</span>
+                    <div
+                        class="text-center my-3"
+                        v-show="showToggleCompletedBtn"
+                    >
+                        <button
+                            class="btn btn-sm btn-secondary"
+                            @click="showCompletedTasks = !showCompletedTasks"
+                        >
+                            <span v-if="!showCompletedTasks"
+                                >Show completed</span
+                            >
+                            <span v-else>Hide completed</span>
                         </button>
-                     </div>
-                    
+                    </div>
+
                     <!-- list of completed tasks -->
-                    <Tasks :tasks="completedTasks" :show="completedTasksIsVisible && showCompletedTasks" />
+                    <Tasks
+                        :tasks="completedTasks"
+                        :show="completedTasksIsVisible && showCompletedTasks"
+                    />
                 </div>
             </div>
         </div>
     </main>
 </template>
-
 <script setup>
-    import { computed, onMounted, ref } from 'vue';
-    import { allTasks } from '../http/task-api';
-    import Tasks from '@/components/tasks/Tasks.vue';
-    const tasks = ref([])
-    onMounted(async()=>{
-        const {data} = await allTasks()
-        tasks.value = data.data
-    })
+import { computed, onMounted, ref } from "vue";
+import { allTasks, createTask } from "../http/task-api";
+import Tasks from "../components/tasks/Tasks.vue";
+import NewTask from "../components/tasks/NewTask.vue";
 
-    const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
-    const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
+const tasks = ref([]);
 
-    const showToggleCompletedBtn = computed(()=> uncompletedTasks.value.length > 0 && completedTasks.value.length > 0)
+onMounted(async () => {
+    const { data } = await allTasks();
+    tasks.value = data.data;
+});
 
-    const completedTasksIsVisible = computed(
-        () => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0
-    )
-    const showCompletedTasks = ref(false)
+const uncompletedTasks = computed(() =>
+    tasks.value.filter((task) => !task.is_completed),
+);
+const completedTasks = computed(() =>
+    tasks.value.filter((task) => task.is_completed),
+);
+const showToggleCompletedBtn = computed(
+    () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0,
+);
+const completedTasksIsVisible = computed(
+    () =>
+        uncompletedTasks.value.length === 0 || completedTasks.value.length > 0,
+);
+const showCompletedTasks = ref(false);
 
+const handleAddedTask = async (newTask) => {
+    const { data: createdTask } = await createTask(newTask);
+    tasks.value.unshift(createdTask.data);
+};
 </script>
