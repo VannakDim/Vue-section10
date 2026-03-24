04b5b2c827279e3d62013318d18d15361024bca7
M1 <studio@M1s-Mac-mini.local>
2026-03-24 11:18:32 +0700

    91-92. Route Navigations

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
   import Navbar from './components/Navbar.vue';
-  import TaskPage from './pages/TaskPage.vue';
   import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Navbar />
-  <TaskPage />
+  <router-view />
   <Footer />
 
 </template>
