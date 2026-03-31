cf7ec71fa65d6fb0b4102c351a99aed0a04f10ad
M1 <studio@M1s-Mac-mini.local>
2026-03-31 18:00:04 +0700

    136. Update task priority

--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -3,8 +3,8 @@
         <div class="d-flex justify-content-start align-items-center">
             <input
                 class="form-check-input mt-0"
-                :class="completedClass"
                 type="checkbox"
+                :class="[completedClass, priorityClass]"
                 :checked="task.is_completed"
                 @change="markTaskAsCompleted"
             />
@@ -12,22 +12,33 @@
                 class="ms-2 flex-grow-1"
                 :class="completedClass"
                 title="Double click the text to edit or remove"
-                @dblclick="$event => isEdit = true"
+                @dblclick="isEdit = true"
             >
                 <div class="relative" v-if="isEdit">
-                    <input class="editable-task" type="text"
-                    v-focus
-                    @keyup.esc="undo"
-                    @keyup.enter="updateTask"
-                    v-model="editingTask"
-                />
+                    <input
+                        class="editable-task"
+                        type="text"
+                        v-focus
+                        @keyup.esc="undo"
+                        @keyup.enter="updateTask"
+                        v-model="editingTask"
+                        ref="inputRef"
+                    />
+                    <div class="select-priority">
+                        <SelectPriority
+                            :selected="selectedPriority" 
+                            @change="setPriority"
+                        />
+                    </div>
                 </div>
                 <span v-else>{{ task.name }}</span>
             </div>
             <!-- <div class="task-date">24 Feb 12:00</div> -->
         </div>
-        <TaskActions @edit="$event => isEdit = true" v-show="!isEdit"
-            @remove="removeTask"    
+        <TaskActions
+            @edit="isEdit = true"
+            v-show="!isEdit"
+            @remove="removeTask"
         />
     </li>
 </template>
@@ -35,42 +46,93 @@
 <script setup>
 import { computed, ref } from "vue";
 import TaskActions from "./TaskActions.vue";
+import SelectPriority from "./SelectPriority.vue";
+
 const props = defineProps({
     task: Object,
 });
 
-const emit = defineEmits(['updated', 'completed', 'removed']);
+const emit = defineEmits(["updated", "completed", "removed"]);
 
-const isEdit = ref(false)
+const inputRef = ref();
+const selectedPriority = ref(props.task.priority?.id || null)
 
-const editingTask = ref(props.task.name)
+const setPriority = (id) => {
+    selectedPriority.value = id
+    inputRef.value.focus();
+}
 
+const isEdit = ref(false);
+const editingTask = ref(props.task.name);
 const completedClass = computed(() =>
     props.task.is_completed ? "completed" : "",
 );
 
 const vFocus = {
-    mounted: (el) => el.focus()
-}
+    mounted: (el) => el.focus(),
+};
 
-const updateTask = event => {
-    const updatedTask = { ...props.task, name: event.target.value }
-    isEdit.value = false
-    emit('updated', updatedTask)
-}
+const updateTask = (event) => {
+    const updatedTask = { 
+        ...props.task, 
+        name: event.target.value,
+        priority_id: selectedPriority.value
+    };
+    isEdit.value = false;
+    emit("updated", updatedTask);
+};
 
-const markTaskAsCompleted = event => {
-    const updatedTask = { ...props.task, is_completed: !props.task.is_completed }
-    emit('completed', updatedTask)
-}
+const undo = () => {
+    isEdit.value = false;
+    editingTask.value = props.task.name;
+    selectedPriority.value = props.task.priority?.id || null
+};
 
-const undo = ()=>
-    isEdit.value = false
-    editingTask.value = props.task.name
+const markTaskAsCompleted = (event) => {
+    const updatedTask = {
+        ...props.task,
+        is_completed: !props.task.is_completed,
+    };
+    emit("completed", updatedTask);
+};
 
-const removeTask = () =>{
-    if(confirm("Are you sure?"))
-        emit('removed', props.task)
-}
+const removeTask = () => {
+    if (confirm("Are you sure?")) {
+        emit("removed", props.task);
+    }
+};
 
+const priorityClass = computed(() => {
+    const classesMap = {
+        null: 'none',
+        1: 'high',
+        2: 'medium',
+        3: 'low'
+    }
+    const activeClass = classesMap[selectedPriority.value] || 'none';
+    return `priority-${activeClass}`;
+});
 </script>
+
+<style scoped>
+.form-check-input:checked {
+    background-color: rgb(108,117,125);
+    border-color: rgb(108,117,125);
+}
+.form-check-input:not(:checked) {
+   outline: 0;
+   border: 0;
+}
+.priority-high:not(:checked) {
+   box-shadow: 0 0 0 0.1rem rgb(220,53,69) !important;
+}
+.priority-medium:not(:checked) {
+   box-shadow: 0 0 0 0.1rem rgb(255,193,7) !important;
+}
+.priority-low:not(:checked) {
+   box-shadow: 0 0 0 0.1rem rgb(13,110,253) !important;
+}
+.priority-none:not(:checked) {
+   box-shadow: 0 0 0 0.1rem rgba(0,0,0,.25) !important;
+}
+</style>
--- src/pages/TaskPage.vue
+++ src/pages/TaskPage.vue
@@ -3,6 +3,12 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8 offset-md-2">
+                    <!-- Task order -->
+                    <div class="d-flex align-items-center justify-content-between mb-3">
+                        <h3 class="text-body mb-0">Tasks</h3>
+                        <SortTasks />
+                    </div>
+
                     <!-- Add new Task -->
                     <NewTask />
 
@@ -10,58 +16,50 @@
                     <Tasks :tasks="uncompletedTasks" />
 
                     <!-- show toggle button -->
-                    <div
-                        class="text-center my-3"
-                        v-show="showToggleCompletedBtn"
-                    >
-                        <button
-                            class="btn btn-sm btn-secondary"
-                            @click="showCompletedTasks = !showCompletedTasks"
-                        >
-                            <span v-if="!showCompletedTasks"
-                                >Show completed</span
-                            >
+                    <div class="text-center my-3" v-show="showToggleCompletedBtn">
+                        <button class="btn btn-sm btn-secondary" @click="showCompletedTasks = !showCompletedTasks">
+                            <span v-if="!showCompletedTasks">Show completed</span>
                             <span v-else>Hide completed</span>
                         </button>
                     </div>
 
                     <!-- list of completed tasks -->
-                    <Tasks
-                        :tasks="completedTasks"
-                        :show="completedTasksIsVisible && showCompletedTasks"
-                    />
+                    <Tasks :tasks="completedTasks" :show="completedTasksIsVisible && showCompletedTasks" />
                 </div>
             </div>
         </div>
     </main>
 </template>
 <script setup>
-import { computed, onMounted, ref } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { storeToRefs } from "pinia";
 import { useTaskStore } from "../stores/task";
 import Tasks from "../components/tasks/Tasks.vue";
 import NewTask from "../components/tasks/NewTask.vue";
+import SortTasks from "../components/tasks/SortTasks.vue";
+import { useRoute } from "vue-router";
 
-const store = useTaskStore()
-const {completedTasks, uncompletedTasks} = storeToRefs(store)
-const {fetchAllTasks} = store
+const store = useTaskStore();
+const { completedTasks, uncompletedTasks } = storeToRefs(store);
+const { fetchAllTasks } = store;
 
+const route = useRoute();
+
+watch(
+    () => route.query,
+    async (query) => await fetchAllTasks(query),
+    { immediate: true },
+);
 
 onMounted(async () => {
-    await fetchAllTasks()
-    
+    await fetchAllTasks();
 });
-
 
 const showToggleCompletedBtn = computed(
     () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0,
 );
 const completedTasksIsVisible = computed(
-    () =>
-        uncompletedTasks.value.length === 0 || completedTasks.value.length > 0,
+    () => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0,
 );
 const showCompletedTasks = ref(false);
-
-
-
 </script>
